b1ba046b714cf23246c72a64dc58fb3993addef0
David Poetzsch-Heffter <david.poetzsch-heffter@twail.net>
2017-02-27 17:24:49 +0100

    first html stats version

--- index.js
+++ index.js
@@ -2,30 +2,78 @@
 
 "use strict"
 
-// Retrieve
 const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+const _ = require("lodash");
+const Mustache = require("mustache");
+const path = require("path");
 
 // const DB_URL = "mongo mongodb://hireme:fawcaukwen0Shnupperk@ds021046.mlab.com:21046/hireme-demo"
 const DB_URL = "mongodb://hireme:SehrGeheim@ds029655.mlab.com:29655/hireme-dev"
 
+const TEMPLATES = {
+    index: fs.readFileSync(__dirname + '/templates/index.mustache', { encoding: "utf8" }),
+    detail: fs.readFileSync(__dirname + '/templates/detail.mustache', { encoding: "utf8" }),
+}
+
+if (process.argv.length <= 2) {
+    const path = process.argv[1].split("/");
+    console.log("Usage: node " + _.last(path) + " <out-dir>");
+    process.exit(1);
+}
+
+const OUT_DIR = process.argv[2]
+if (!fs.existsSync(OUT_DIR)) {
+    fs.mkdirSync(OUT_DIR);
+}
+
 // Connect to the db
 MongoClient.connect(DB_URL).then((db) => {
-  const prof = db.collection("system.profile");
+    console.log("connected");
+
+    const prof = db.collection("system.profile");
   
-  return new Promise(resolve => {
-    const stream = prof.find().stream();
-    
-    stream.on("data", (q) => {
-        if (q.docsExamined !== q.nreturned) {
-            console.log(`${q.ns}   --   ${q.nreturned} / ${q.docsExamined}`);
-            console.log(q.query);
-            console.log();
-        }
+    process.stdout.write("Fetching data");
+
+    return new Promise(resolve => {
+        const stream = prof.find().stream();
+        
+        const res = []
+
+        stream.on("data", (q) => {
+            process.stdout.write(".");
+            if (q.docsExamined !== q.nreturned) {
+                res.push(q);
+            }
+        });
+        
+        stream.on("end", () => {
+            resolve(res);
+        });
     });
-    
-    stream.on("end", () => resolve());
-  });
+}).then(queries => {
+    console.log()
+    process.stdout.write("Writing Files");
+
+    queries = _.sortBy(queries, q => q.nreturned - q.docsExamined)
+
+    const idxFile = OUT_DIR + "/index.html"
+    fs.writeFileSync(idxFile, Mustache.render(TEMPLATES.index, {
+        queries: queries
+    }));
+
+    process.stdout.write(".");
+
+    for (const q of queries) {
+        q.raw = JSON.stringify(q, null, 4);
+        fs.writeFileSync(`${OUT_DIR}/${q.ns}.html`, Mustache.render(TEMPLATES.detail, q));
+        process.stdout.write(".");
+    }
+
+    console.log();
+    console.log(`Open: sensible-browser file://${path.resolve(idxFile)}`)
 }).then(() => {
+    console.log("Finished.");
     process.exit(0)
 }).catch(e => {
     console.error(e);
